f6576daf011691ce07b0f3da0c707df608184f04
Nathan Brown <scottynate05@yahoo.com>
2020-10-29 20:05:21 -0700

    demo 4

--- src/demonstration_04.py
+++ src/demonstration_04.py
@@ -7,22 +7,55 @@ corresponding emoticons.
 
 word -> emoticon
 ---
-smile -> :D
-grin -> :)
+smile -> :)
+grin -> :D
 sad -> :(
 mad	-> :P
 
 Examples:
-- emotify("Make me smile") ➞ "Make me :D"
-- emotify("Make me grin") ➞ "Make me :)"
+- emotify("Make me smile") ➞ "Make me :)"
+- emotify("Make me grin") ➞ "Make me :D"
 - emotify("Make me sad") ➞ "Make me :("
 
 Notes:
 - The sentence always starts with "Make me".
 - Try to solve this without using conditional statements like if/else.
+
+- We might want to use a dictionary to store the key, value pairs
+{
+    'smile': ':)'
+    'grin': ':D'
+    'sad': ':('
+    'mad': ':P'
+}
+
+- iterate over the hash table's items extrapolating the key and value of each item
+- use a string '.replace' to replace the substring of the key with the value
+
 """
 
 
 def emotify(txt):
-    # Your code here
+    # store the key, value pair of the required data for swapping out the test to emoticons
+    # we will store this data in a hash table (dictionary = Python | object = JS)
+    data = {
+        'smile': ':)',
+        'grin': ':D',
+        'sad': ':(',
+        'mad': ':P',
+    }
+    # iterate over the hash table's items extracting the key and value
+    for k, v in data.items():
+        # set an updated string using the '.replace' method
+        # return each txt with the old substring substituted for the old one
+        txt = txt.replace(k, v)
 
+    # return our new txt
+    return txt
+
+
+
+print(emotify("Make me smile")) # ➞ "Make me :)"
+print(emotify("Make me grin")) # ➞ "Make me :D"
+print(emotify("Make me sad")) # ➞ "Make me :("
+print(emotify("Make me mad")) # ➞ "Make me :P"
