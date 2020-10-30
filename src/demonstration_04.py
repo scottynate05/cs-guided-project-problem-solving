8907f4a8198f85b2bef075ad10ea2572a9cf7793
Nathan Brown <scottynate05@yahoo.com>
2020-10-29 20:24:22 -0700

    demo 4 complete

--- src/demonstration_04.py
+++ src/demonstration_04.py
@@ -36,6 +36,25 @@ Notes:
 
 
 def emotify(txt):
+    # first pass solution - Time Complexity O(n)
+    # store the key, value pair of the required data for swapping out the test to emoticons
+    # we will store this data in a hash table (dictionary = Python | object = JS)
+    # data = {
+    #     'smile': ':)',
+    #     'grin': ':D',
+    #     'sad': ':(',
+    #     'mad': ':P',
+    # }
+    # # iterate over the hash table's items extracting the key and value
+    # for k, v in data.items():
+    #     # set an updated string using the '.replace' method
+    #     # return each txt with the old substring substituted for the old one
+    #     txt = txt.replace(k, v)
+
+    # # return our new txt
+    # return txt
+
+    # second pass - Time Complexity O(1)
     # store the key, value pair of the required data for swapping out the test to emoticons
     # we will store this data in a hash table (dictionary = Python | object = JS)
     data = {
@@ -44,16 +63,15 @@ def emotify(txt):
         'sad': ':(',
         'mad': ':P',
     }
-    # iterate over the hash table's items extracting the key and value
-    for k, v in data.items():
-        # set an updated string using the '.replace' method
-        # return each txt with the old substring substituted for the old one
-        txt = txt.replace(k, v)
 
-    # return our new txt
-    return txt
-
-
+    # get the substring of txt from the start to index 7 (8 - 1) indices in [:8] set it as start_of_string => 'Make me '
+    start_of_string = txt[ :8]
+    # get the data at 8 in from the start of the string [8: ] set it as end_of_string => 'smile'
+    end_of_string = txt[8: ]
+    # concatinate start_of_string with value from data using end_of_string => 'smile' => ':)' as the key and set them to full_string
+    full_string = start_of_string + data[end_of_string]
+    # return full_string
+    return full_string
 
 print(emotify("Make me smile")) # ➞ "Make me :)"
 print(emotify("Make me grin")) # ➞ "Make me :D"
